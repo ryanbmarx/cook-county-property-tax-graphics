7adeb7702b72034dbe89c590ea58542d8b9a87b7
ryanbmarx <ryanbmarx@gmail.com>
2017-03-23 17:34:59 -0500

    Pre-build gauge charts this way.

---
+++ js/src/scatterline-square.js
@@ -0,0 +1,115 @@
+import * as d3 from 'd3';
+import getTribColors from './getTribColors.js'
+
+// This allows iteration over an HTMLCollection (as I've done in setting the checkbutton event listeners,
+// as outlined in this Stack Overflow question: http://stackoverflow.com/questions/22754315/foreach-loop-for-htmlcollection-elements
+// NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+// HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+
+
+// (data, category, container)
+
+function styleFeature(featureFillColor){
+	// http://leafletjs.com/reference.html#path-options
+	return {
+		color: "#eee",
+		stroke:true,
+		weight:1,
+		fillColor:featureFillColor,
+		className:'tract',
+		fillOpacity: .9
+	};
+}
+
+function onEachFeature(feature, layer){
+	// Everything in this function is executed on each feature.
+	layer.id = feature.properties.NAME10;
+	layer.bindPopup(feature.properties.NAMELSAD10);
+}
+
+
+
+class scatterlineSquare{
+	constructor(options){
+
+		let app = this;
+		app.options = options;
+
+		const bbox = d3.select(app.options.container).node().getBoundingClientRect(),
+				circleRadius = app.options.circleRadius,
+				height = bbox.height,
+				width = bbox.width,
+				margin = app.options.margin,
+				innerHeight = height - margin.top - margin.bottom,
+				innerWidth = width - margin.right - margin.left,
+				data = app.options.data,
+				category = app.options.categoryToChart,
+				rectWidth = app.options.rectWidth,
+				rectHeight = app.options.rectHeight;
+
+		const axisFormat = "";
+
+		const extent = d3.extent(data, d => parseFloat(d[category]));
+			
+		const scatterScale = d3.scaleLinear()
+			.domain(extent)
+			.range([0,innerWidth])
+			.nice();
+
+		// console.log(scatterScale, extent);
+
+		const xAxis = d3.axisBottom(scatterScale)
+		    .tickSize((rectHeight / 2) + 5)
+		    .tickFormat(d3.format(axisFormat));
+
+
+		// ----------------------------------
+		// START MESSING WITH SVGs
+		// ----------------------------------
+
+		//Inserts svg and sizes it
+		const svg = d3.select(app.options.container)
+			.append("svg")
+	        .attr("width", width)
+	        .attr("height", height);
+
+		const chartInner = svg.append('g')
+			.classed('chartInner', true)
+			.attr("width",innerWidth)
+			.attr("height",innerHeight)
+			.attr(`transform`,`translate(${margin.left},${margin.top})`);
+
+		// svg.append('g')
+		// 	.classed('x', true)
+		// 	.classed('axis', true)
+		// 	.attr(`transform`,`translate(${margin.left},${margin.top + (innerHeight / 2)})`)
+		// 	.call(xAxis)
+		// 	.selectAll('text').remove();
+
+		
+		chartInner.append('line')
+			.classed('scatterline__line', true)
+			.attr('x1', 0)
+			.attr('x2', innerWidth)
+			.attr('y1', innerHeight / 2)
+			.attr('y2', innerHeight / 2)
+			.style('stroke', 'black')
+			.style('stroke-width', 1);
+
+		chartInner.selectAll('rect')
+			.data(data)
+			.enter()
+				.append('rect')
+				.classed('scatterline__square', true)
+				.style('fill', getTribColors('trib-blue2'))
+				.style('opacity', .3)
+				.attr('width', rectWidth)
+				.attr('height', rectHeight)
+				.attr('x', d => scatterScale(d[category]) - (rectWidth / 2))
+				.attr('y', (innerHeight - rectHeight) / 2)
+				.attr('data-value', d => d[category]);
+	}
+	
+}
+
+module.exports = scatterlineSquare;
---
+++ scripts/render2.js
@@ -0,0 +1,61 @@
+var system = require('system'),
+  	fs = require('fs'),
+    webPage = require('webpage'),
+    page = webPage.create();
+
+//http://stackoverflow.com/questions/12555203/phantomjs-does-not-execute-function-in-page-evaluate-function
+page.onConsoleMessage = function (msg) { console.log(msg); };
+
+// Ripped from http://stackoverflow.com/questions/26609450/phantomjs-page-open-freezes
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
+
+
+page.onError = function (msg, trace) {
+	// Ripped from http://phantomjs.org/troubleshooting.html
+    console.log(msg);
+    trace.forEach(function(item) {
+        console.log('  ', item.file, ':', item.line);
+    });
+};
+
+
+page.open('http://graphics.chicagotribune.com/property-tax-assessments-map/gauges.html', function(status){
+	console.log('status: ', status);
+	if (status == "success"){
+		setTimeout(function(){
+			try{
+				console.log('Rendering image');
+				page.render('screen.png');
+			}
+			catch (e){
+				console.log('Error rendering screenshot!');
+				console.log(e);
+			}
+
+			var code = page.evaluate( function(){
+				var elements = document.querySelectorAll("div");
+				
+				// Will house the code I want to make seperate files out of
+				var code = [];
+
+				// Loop through and find each chart, stashing it's SVG into an array of objects
+				for (var i  = 0; i < elements.length; i++) {
+					code.push({
+						id:elements[i].id,
+						code:elements[i].innerHTML
+					})
+				}
+				return code;
+			});
+			
+			console.log(code);
+			for (var i=0; i<code.length; i++){
+				console.log(i, code[i].id);
+				fs.write("img/src/"+code[i].id + ".svg", code[i].code, 'w');
+			}
+			phantom.exit();
+		}, 10000)
+	} else {
+		console.log("Error loading page. Maybe your server isn't running?");
+	}
+})
