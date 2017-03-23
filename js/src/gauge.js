367c3be2e6d776b341218015d8cf8ba516d6e2a6
ryanbmarx <ryanbmarx@gmail.com>
2017-03-23 17:36:55 -0500

    With more gauges, make them smaller.

--- js/src/gauge.js
+++ js/src/gauge.js
@@ -1,16 +1,13 @@
 import * as d3 from 'd3';
 import scatterlineSquare from './scatterline-square.js';
 
-
 window.onload = function(){
-
-
 	d3.json(`http://${window.ROOT_URL}/data/tract-data2.geojson`, (err, data) => {
 		if (err) throw err;
 
 		const features = data.features;
 
-		// flatten our data
+		// flatten our data by removing all the geo stuff.
 			
 		let scatterlineData = [];
 
@@ -20,11 +17,11 @@ window.onload = function(){
 
 
 		const divs = document.querySelectorAll('div');
-		console.log(divs);
+		// console.log(divs);
 		for (var i=0; i< divs.length; i++){
 			const div = divs[i];
 			const category = div.dataset.chart;
-			console.log(scatterlineData);
+			// console.log(scatterlineData);
 
 			const gauge = new scatterlineSquare({
 				container: div,
